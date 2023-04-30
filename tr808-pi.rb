581ddb144e5855cd6995ec61f70882f5885a2cdb
nischalshrestha <bitsorific@gmail.com>
2023-04-30 15:49:03 -0400

    feat: patterns can be named, represented as Hash now

--- tr808-pi.rb
+++ tr808-pi.rb
@@ -1,8 +1,7 @@
-
 # global map for sample to:
 # - path (location of sample dir)
 # - version (which file to pick for sample when there are multiple)
-# - hits (whether to play the instrument or not)
+# - hits (Hash<String, List> where keys represent names of patterns, and List is hits)
 # then some custom params for sample arguments like BD_amp for amp
 # which will initially set by `setup_samples()`
 $samples_map = {}
@@ -41,13 +40,15 @@ def setup_samples()
     $samples_map[sym_key] = {
       :path => path,
       :version => 1, # which file to pick for samples
-      :hits => []
+      # hits is a Hash that looks like this:
+      # {"A": [0..n], "B": [0..n], ... }
+      :hits => {}
     }
     set amp_param, 1.5
   end
 end
 
-def inst_to_binary(splitlines)
+def inst_to_binary(splitlines, pattern_name=0)
   # first, we map each instrument to its hit pattern
   inst_to_hits = splitlines.map{
     |line|
@@ -61,9 +62,10 @@ def inst_to_binary(splitlines)
     return splits.map(&:to_i)
   end
 
-  # then, create a dictionary of inst => [0s, 1s]
+  # make a Hash out of the list pair ["inst", [0..n]]
+  # such that values represent named pattern
   return inst_to_hits.map{
-    |k, v| [k.to_sym, make_hits(v)]
+    |k, v| [k.to_sym, {pattern_name => make_hits(v)}]
   }.to_h
 end
 
@@ -72,16 +74,18 @@ def parse_beat(src)
   Return a dictionary representing a beat pattern for the TR-808.
 
   Args:
-      src (Integer or Array): The string(s) representing the beat pattern.
+      src (String, Array<String>, Hash<String, String>): 
+      The string(s) representing the beat pattern.
 
   Returns:
       The dictionary.
   """
   setup_samples()
 
+  # helper function to update the $samples_map
   def set_hits(hits)
-    hits.each do |k, v|
-      $samples_map[k][:hits].push(v)
+    hits.each do |inst, hits_dict|
+      $samples_map[inst][:hits] = hits_dict
     end
   end
 
@@ -92,11 +96,46 @@ def parse_beat(src)
     set_hits(hits)
   elsif src.class == Array
     # for multiple patterns, create hits for each pattern
-    src.each do |pattern|
+    final_hits = {}
+    src.each_with_index do |pattern, index|
       pattern = pattern.gsub(/^\s+/, '').strip
-      hits = inst_to_binary(pattern.split("\n"))
-      set_hits(hits)
+      # by default, pattern for each pattern is the index
+      hits = inst_to_binary(pattern.split("\n"), pattern_name = index)
+
+      # initialize the final hits hash if it's the first entry
+      if final_hits.length == 0
+        final_hits = hits
+      else
+        # otherwise, update the hits hash of each instrument
+        # in final_hits so we add more patterns
+        hits.each do |name, pattern|
+          # pp final_hits[name]
+          final_hits[name] = final_hits[name].merge(pattern)
+        end
+      end
     end
+    
+    set_hits(final_hits)
+  elsif src.class == Hash
+    # for multiple named patterns, create hits for each pattern with 
+    # name specified by user
+    final_hits = {}
+    src.each do |name, pattern|
+      pattern = pattern.gsub(/^\s+/, '').strip
+      hits = inst_to_binary(pattern.split("\n"), pattern_name = name)
+
+      # initialize the final hits hash if it's the first entry
+      if final_hits.length == 0
+        final_hits = hits
+      else
+        # otherwise, update the hits hash of each instrument
+        # in final_hits so we add more patterns
+        hits.each do |name, pattern|
+          final_hits[name] = final_hits[name].merge(pattern)
+        end
+      end
+    end
+    set_hits(final_hits)
   end
 end
 
@@ -125,14 +164,23 @@ end
 
 def tr808(src, bpm: 90, pattern: [0])
   """
-  Play a live loop of the TR-808 given a beat pattern and bpm.
+  Play a live loop of the TR-808 given a beat pattern(s) and bpm.
 
   Args:
-      src (String): The string representing the TR-808 beat pattern.
+      src (String, Array<String>, Hash<String, String>): 
+        The string(s) representing the TR-808 beat pattern(s).
+        
+        For 1 pattern, provide a String for the pattern. For multiple
+        patterns that are not named, provide an Array of Strings.
+        
+        If you want to name each pattern, provide a Hash with the key
+        representing the name of pattern and the value as the String pattern.
       bpm (Integer): The bpm, 90 by default.
-      pattern (Array): An array of which pattern to play if there are 
-        multiple patterns, [0] by default for one single pattern
-
+      pattern (Array<Integer>, Array<String>): 
+        An array of which pattern to play if there are 
+        multiple patterns, [0] by default for one single pattern. 
+        
+        For specifying names of the pattern provide an Array of String(s).
   Returns:
       nil
   """
@@ -148,7 +196,7 @@ def tr808(src, bpm: 90, pattern: [0])
       # for each instrument, play the sample if it's a hit
       $samples_map.each do |inst, values|
         hits = values[:hits]
-        if hits.length && cur_pattern < hits.length && hits[cur_pattern][i] == 1
+        if hits.length && hits.has_key?(cur_pattern) && hits[cur_pattern][i] == 1
           sample_tr808(inst)
         end
       end
